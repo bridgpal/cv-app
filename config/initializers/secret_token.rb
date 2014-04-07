cfdcc21332a80311c404a0ac234990fcf0ddf738
Anil Bridgpal <bridgpal@gmail.com>
2014-04-07 11:25:17 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CvApp::Application.config.secret_key_base = 'af9ac95d6c7d7c429414f0f6583b9ffe10725024f926d750d578edc1145f5291c709593efc505b28e393ba97f337cc2b21f5112493d7f79e22d8fce4e46609b8'
